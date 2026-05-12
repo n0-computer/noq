9272b56dfa448cfbd4139017208510d7d7c47694
Philipp Krüger <philipp.krueger1@gmail.com>
2026-05-12 16:14:17 +0200

    Make sure to check PathEvents are handled on test level at least

--- noq/src/connection.rs
+++ noq/src/connection.rs
@@ -1684,6 +1684,10 @@ impl State {
                     // PathEvent is #[non_exhaustive].
                     // It's possible that noq is built against a newer noq-proto version.
                     // In that case, we need to ignore path events we can't handle yet.
+                    // But for tests, we expect noq and noq-proto to be in sync, so we
+                    // should panic in case we don't actually handle new cases.
+                    #[cfg(test)]
+                    panic!("Unhandled PathEvent variant: {event:?}");
                 }
             }
         }
