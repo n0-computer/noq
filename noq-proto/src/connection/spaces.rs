c39c57eb8ad5a9a88110c16ea4d6816f944b30a3
Floris Bruynooghe <flub@n0.computer>
2026-06-10 21:33:42 +0200

    impl retransmit of RESET_STREAM_AT

--- noq-proto/src/connection/spaces.rs
+++ noq-proto/src/connection/spaces.rs
@@ -558,6 +558,7 @@ pub struct Retransmits {
     pub(super) remove_address: BTreeSet<RemoveAddress>,
     /// Round and local addresses to advertise in `REACH_OUT` frames
     pub(super) reach_out: PendingReachOutFrames,
+    pub(super) reset_stream_at: Option<u64>,
 }
 
 impl Retransmits {
@@ -584,6 +585,7 @@ impl Retransmits {
             add_address,
             remove_address,
             reach_out,
+            reset_stream_at,
         } = &self;
         !max_data
             && !max_stream_id.iter().any(|x| *x)
@@ -608,6 +610,7 @@ impl Retransmits {
             && add_address.is_empty()
             && remove_address.is_empty()
             && reach_out.is_empty()
+            && reset_stream_at.is_none()
     }
 }
 
@@ -635,6 +638,7 @@ impl ::std::ops::BitOrAssign for Retransmits {
             add_address,
             remove_address,
             mut reach_out,
+            reset_stream_at,
         } = rhs;
 
         // We reduce in-stream head-of-line blocking by queueing retransmits before other data for
@@ -664,6 +668,12 @@ impl ::std::ops::BitOrAssign for Retransmits {
         self.add_address.extend(add_address.iter().copied());
         self.remove_address.extend(remove_address.iter().copied());
         self.reach_out.append(&mut reach_out);
+        self.reset_stream_at = match (self.reset_stream_at, reset_stream_at) {
+            (None, None) => None,
+            (None, Some(v)) => Some(v),
+            (Some(v), None) => Some(v),
+            (Some(l), Some(r)) => Some(l.min(r)),
+        };
     }
 }
 
