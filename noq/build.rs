fcd420c02e03cd0bc620f4345a590f4a8b8ae88a
Philipp Krüger <philipp.krueger1@gmail.com>
2026-07-16 14:02:23 +0200

    Allow `semicolon_in_expressions_from_macros`

--- noq/build.rs
+++ noq/build.rs
@@ -1,5 +1,9 @@
 use cfg_aliases::cfg_aliases;
 
+#[allow(
+    semicolon_in_expressions_from_macros,
+    reason = "cfg_aliases needs an update: https://github.com/katharostech/cfg_aliases/pull/15"
+)]
 fn main() {
     // Setup cfg aliases
     cfg_aliases! {
