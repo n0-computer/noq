8d06fdf21ae8f40ef4cd8d344f9aea0bd5ca4bfa
Floris Bruynooghe <flub@n0.computer>
2026-07-29 17:26:30 +0200

    fix(udp): Disable SO_TIMESTAMPNS for now (#776)
    ## Description
    
    We do not rely on this, and the implementation is broken on musl.
    
    Fixes #774.
    
    
    ## Breaking Changes
    
    n/a
    
    ## Notes & open questions
    
    We need to run tests on musl in CI somehow.
    
    ## Change checklist
    
    - [x] Self-review.
    - [x] Tests if relevant.
    - [x] This PR was created by a human that thought critically about the
          proposed change and wrote an as clear and concise description as
          they could.
    - [x] This PR isn't slop, and is carefully crafted to do have the
          intented effect.
    - [x] `cargo make` passes locally.
    
    ---------
    
    Co-authored-by: dignifiedquire <me@dignifiedquire.com>

--- noq-udp/tests/tests.rs
+++ noq-udp/tests/tests.rs
@@ -473,25 +473,27 @@ fn test_send_recv(send: &Socket, recv: &Socket, transmit: Transmit<'_>) {
             assert_eq!(meta.ecn, transmit.ecn);
         }
 
+        // Disable SO_TIMESTAMPNS for now: https://github.com/n0-computer/noq/issues/774
+        assert!(meta.timestamp.is_none());
         // On Linux and Android, we expect the kernel to provide a receive timestamp
         // since we explicitly enabled `SO_TIMESTAMPNS`.
-        #[cfg(all(any(target_os = "linux", target_os = "android"), not(posix_minimal)))]
-        {
-            assert!(
-                meta.timestamp.is_some(),
-                "Kernel timestamp should be present on Linux/Android"
-            );
-            assert!(
-                meta.timestamp.unwrap() > std::time::Duration::ZERO,
-                "Kernel timestamp should be non-zero"
-            );
-        }
+        // #[cfg(all(any(target_os = "linux", target_os = "android"), not(posix_minimal)))]
+        // {
+        //     assert!(
+        //         meta.timestamp.is_some(),
+        //         "Kernel timestamp should be present on Linux/Android"
+        //     );
+        //     assert!(
+        //         meta.timestamp.unwrap() > std::time::Duration::ZERO,
+        //         "Kernel timestamp should be non-zero"
+        //     );
+        // }
 
-        // On other platforms, the timestamp should remain `None`.
-        #[cfg(not(any(target_os = "linux", target_os = "android", posix_minimal)))]
-        {
-            assert!(meta.timestamp.is_none());
-        }
+        // // On other platforms, the timestamp should remain `None`.
+        // #[cfg(not(any(target_os = "linux", target_os = "android", posix_minimal)))]
+        // {
+        //     assert!(meta.timestamp.is_none());
+        // }
     }
     assert_eq!(datagrams, expected_datagrams);
 }
